9b230123dabcecd9f840e545f64f2e997a6fd485
arnob016 <arnob.social@gmail.com>
2024-03-01 13:56:29 +0600

    text font size fix

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -9,15 +9,15 @@ const Logo: React.FC = () => {
     <div className="flex flex-col border-2">
       <div className="flex w-full justify-center items-center">
         <img src={logoPath} alt="Logo" className='w-1/4 p-2' />
-        <Link href="https://aidea.com" className='font-bold text-xl ml-2 bg-gradient-to-r from-fuchsia-500 to-cyan-500 inline-block bg-clip-text text-transparent'>AIdea</Link>
+        <Link href="https://aidea.com" className='font-bold text-4xl ml-2 bg-gradient-to-r from-fuchsia-500 to-cyan-500 inline-block bg-clip-text text-transparent'>AIdea</Link>
       </div>
       <div className="flex w-full justify-center items-center">
         <img src={logoPath} alt="Logo" className='w-1/4 p-2' />
-        <Link href="https://aidea.com" className='font-bold text-xl ml-2 bg-gradient-to-r from-violet-600 to-indigo-600 inline-block bg-clip-text text-transparent'>AIdea</Link>
+        <Link href="https://aidea.com" className='font-bold text-4xl ml-2 bg-gradient-to-r from-violet-600 to-indigo-600 inline-block bg-clip-text text-transparent'>AIdea</Link>
       </div>
       <div className="flex w-full justify-center items-center">
         <img src={logoPath} alt="Logo" className='w-1/4 p-2' />
-        <Link href="https://aidea.com" className='font-bold text-xl ml-2 bg-gradient-to-r from-blue-600 to-violet-600 inline-block bg-clip-text text-transparent'>AIdea</Link>
+        <Link href="https://aidea.com" className='font-bold text-4xl ml-2 bg-gradient-to-r from-blue-600 to-violet-600 inline-block bg-clip-text text-transparent'>AIdea</Link>
       </div>
     </div>
   );
