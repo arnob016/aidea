ba26802aa46ffdb7e0fda3339ae07881626a16e6
Redul94 <redul15-3193@diu.edu.bd>
2024-03-03 19:22:19 +0600

    basic structure

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const Logo: React.FC = () => {
-  const logoPath = '/images/aidea.png';
-        
+  const logoPath = "/images/aidea.png";
 
   return (
     <div className="flex flex-col border-2">
-      <div className="flex w-full justify-center items-center">
-        <img src={logoPath} alt="Logo" className='w-1/4 p-2' />
-        <Link href="https://aidea.com" className='font-bold text-4xl ml-2 bg-gradient-to-r from-fuchsia-500 to-cyan-500 inline-block bg-clip-text text-transparent'>AIdea</Link>
+      <div className="flex w-full items-center justify-center">
+        <img src={logoPath} alt="Logo" className="w-1/4 p-2" />
+        <Link
+          href="https://aidea.com"
+          className="ml-2 inline-block bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-4xl font-bold text-transparent"
+        >
+          AIdea
+        </Link>
       </div>
-      <div className="flex w-full justify-center items-center">
-        <img src={logoPath} alt="Logo" className='w-1/4 p-2' />
-        <Link href="https://aidea.com" className='font-bold text-4xl ml-2 bg-gradient-to-r from-violet-600 to-indigo-600 inline-block bg-clip-text text-transparent'>AIdea</Link>
+      <div>
+        <button className="btn btn-neutral ">Neutral</button>
       </div>
-      <div className="flex w-full justify-center items-center">
-        <img src={logoPath} alt="Logo" className='w-1/4 p-2' />
-        <Link href="https://aidea.com" className='font-bold text-4xl ml-2 bg-gradient-to-r from-blue-600 to-violet-600 inline-block bg-clip-text text-transparent'>AIdea</Link>
+      <div className="flex">
+          <Link href="/price">
+            <button className="rounded-lg btn bg-yellow-800 p-2 mr-2 text-white">
+              Subcription
+            </button>
+          </Link>
+
+          
+        </div>
+      <div>
+        <li>
+          <a>Company</a>
+          <ul>
+            <li>
+              <a>About us</a>
+            </li>
+            <li>
+              <a>Contact us</a>
+            </li>
+            <li>
+              <a>Privacy policy</a>
+            </li>
+            <li>
+              <a>Press kit</a>
+            </li>
+          </ul>
+        </li>
       </div>
     </div>
   );
---
+++ src/components/mainpage.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+const MainPage: React.FC = () => {
+  const [inputText, setInputText] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const handleGenerateClick = () => {
+    console.log("Generated:", inputText);
+  };
+
+  return (
+    <>
+      <div className="flex h-screen flex-col space-y-4 p-4">
+        <div className="flex h-1/3 justify-center">
+          <input
+            className="input input-lg input-bordered h-full w-3/5 max-w-xs bg-white"
+            type="text"
+            placeholder="Enter prompt"
+            value={inputText}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="flex justify-center">
+          <button
+            onClick={handleGenerateClick}
+            className="btn w-1/5 rounded-lg bg-cyan-600 p-2"
+          >
+            Generate
+          </button>
+        </div>
+
+        <div className="mt-4 flex h-1/3 w-1/5 flex-col items-center justify-center rounded-2xl border-4 ">
+          {inputText && (
+            <>
+              <div className="flex flex-grow flex-col items-center justify-center p-2">
+                <p className="text-center text-lg">{inputText}</p>
+              </div>
+              <div className="flex justify-center p-2">
+                <button className="btn">Post</button>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
